094d7054b06296025ffd368766e24494f2365974
pisekchai <pisekchaiit@gmail.com>
2021-06-03 22:15:28 +0700

    Test-ic-drive-Bynandit123
    Test-ic-drive-Bynandit123

---
+++ README.md
@@ -0,0 +1,73 @@
+## IC Drive
+
+![Compatibility](https://img.shields.io/badge/compatibility-0.6.25-blue)
+
+IC Drive is a storage application built on the internet computer, which offers the users a decentralized alternate to the current web2 storage services like dropbox, google drive, etc. <br>
+
+**Current State of the application ->**
+- UI is on ReactJS
+- Backend is in motoko
+- User can upload an image on the react frontend that is stored in the canister. The uploaded image is then served on the frontend via the canister call from frontend.
+
+![alt text](https://github.com/IC-Drive/ic-drive/blob/master/dashboard.png?raw=true)
+
+## Prerequisites
+
+Verify the following before running this demo:
+
+*  You have downloaded and installed [Node.js](https://nodejs.org).
+
+*  You have downloaded and installed the [DFINITY Canister
+   SDK](https://sdk.dfinity.org).
+
+*  You have stopped any Internet Computer or other network process that would
+   create a port conflict on 8000.
+
+## Demo
+
+1. Start a local internet computer.
+
+   ```text
+   dfx start
+   ```
+
+1. Open a new terminal window.
+
+1. Reserve an identifier for your canister.
+
+   ```text
+   dfx canister create --all
+   ```
+
+1. Build your front-end.
+
+   ```text
+   npm install
+   ```
+
+1. Build your canister.
+
+   ```text
+   dfx build
+   ```
+
+1. Deploy your canister.
+
+   ```text
+   dfx canister install --all
+   ```
+
+1. Take note of the URL at which the canister is accessible.
+
+   ```text
+   echo "http://localhost:8000/?canisterId=$(dfx canister id www)"
+   ```
+
+1. Open the aforementioned URL in your web browser.
+
+## Current web2 alternates are broken
+- They are susceptible to many types of hacks
+- They have backdoor access to user data
+- Privacy invading algorithms
+
+![alt text](https://github.com/IC-Drive/ic-drive/blob/master/broken-web2-services.png?raw=true)
---
+++ src/icdrive_assets/components/SideBar/SideBar.jsx
@@ -0,0 +1,131 @@
+import React from "react";
+import styled from 'styled-components';
+
+// custom imports
+import icdrive from 'ic:canisters/icdrive';
+import {useUploadFile} from './File.jsx';
+
+// 3rd party imports
+import { CloudUploadOutlined, ShareAltOutlined, SyncOutlined, BookOutlined } from '@ant-design/icons';
+
+const SideBar = () =>{
+
+  const [id, setId] = React.useState("");
+  
+  React.useEffect(async()=>{
+    const id = await icdrive.getOwnId();
+    setId(id);
+  },[])
+
+  const onFileSelect = async (evt) => {
+    console.log(evt.target.files)
+    const file_list = evt.target.files
+    for(let i=0; i<file_list.length; i++){
+      const file = file_list[i];
+      //console.log(file)
+      let _ = await useUploadFile(id, file)
+    }
+  }
+
+  return(
+    <Style>
+      <div className="container">
+        <div className="content">
+          <div className="element">
+            <div className="element-section">
+              <div className="icon-part">
+                <CloudUploadOutlined style={{ fontSize: '25px', color: '#fff' }} />
+              </div>
+              <div className="text-part">
+                <span>
+                  <div>
+                    <label id="label-file" for="upload-file">Upload</label>
+                    <input type="file" id="upload-file" onChange={onFileSelect} className="file_upload" multiple/>
+                  </div>
+                </span>
+              </div>
+            </div>
+          </div>
+
+          <div className="element">
+            <div className="element-section">
+              <div className="icon-part">
+                <ShareAltOutlined style={{ fontSize: '25px', color: '#fff' }} />
+              </div>
+              <div className="text-part">
+                <span>Share</span>
+              </div>
+            </div>
+          </div>
+
+          <div className="element">
+            <div className="element-section">
+              <div className="icon-part">
+                <BookOutlined style={{ fontSize: '25px', color: '#fff' }} />
+              </div>
+              <div className="text-part">
+                <span>Marked</span>
+              </div>
+            </div>
+          </div>
+
+          <div className="element">
+            <div className="element-section">
+              <div className="icon-part">
+                <SyncOutlined style={{ fontSize: '25px', color: '#fff' }} />
+              </div>
+              <div className="text-part">
+                <span>Import Data</span>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </Style>
+  )
+}
+
+export default SideBar;
+
+const Style = styled.div`
+  .container{
+    background: #141C25;
+    height: calc(100vh - 50px);
+    width: 225px;
+  }
+  .content{
+    padding-top: 20px;
+  }
+  .element{
+    padding: 7.5px 15px 7.5px 10px;
+  }
+  .element-section{
+    color: #fff;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+  }
+  .text-part{
+    font-size: 18px;
+    padding-left: 20px;
+  }
+  .element-section:hover{
+    cursor: pointer;
+  }
+  .file_upload {
+    opacity: 0.0;
+    -ms-filter: "progid:DXImageTransform.Microsoft.Alpha(Opacity=0)";
+    filter: alpha(opacity=0);
+    -moz-opacity: 0.0;
+    -khtml-opacity: 0.0;
+  }
+  #upload-file {
+    opacity: 0;
+    position: absolute;
+    z-index: -1;
+  }
+  #label-file:hover {
+    cursor: pointer;
+  }
+`
